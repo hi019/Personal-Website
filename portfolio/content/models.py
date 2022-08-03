a4f6c39c365d7862cdf9e3e7d866269a0b1ec0ed
Amaan Javed <xx_hype_beast_xx@ACcept-the-airdrop.local>
2022-08-02 23:09:06 -0400

    Add Subscribe Form

--- portfolio/content/models.py
+++ portfolio/content/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+ 
+
+class Subscriber(models.Model):
+    name = models.CharField(max_length=128)
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return f"{self.name} : {self.email}"
--- portfolio/content/views.py
+++ portfolio/content/views.py
@@ -1,11 +1,20 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib import admin
+from django.urls import reverse
 from pyparsing import opAssoc
 from django.core.mail import send_mail, BadHeaderError
+from .models import Subscriber
+from django import forms
+from django.forms import ModelForm, ValidationError
 
+
+class EmailForm(ModelForm):
+    class Meta: 
+        model = Subscriber
+        fields = ["name", "email"]
 
 def index(request):
     return render(request, "content/index.html")
@@ -17,4 +26,18 @@ def resume(request):
     return render(request, "content/resume.html")
 
 def contact_me(request):
-     return render(request, "content/contact.html")
+    if request.method == "GET":
+     return render(request, "content/contact.html", {
+         "form": EmailForm
+     })
+    else:
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            Subscriber(name=name, email=email).save()
+            return HttpResponseRedirect(reverse("contact_me"))
+        else: 
+            return render(request, "content/contact.html", {
+            "form": form,
+        })
